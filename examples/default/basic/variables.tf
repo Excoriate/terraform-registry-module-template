348a1d55ae95a800d660ebdd51950d6623b72415
Alex ATorres <atorres.ruiz@hotmail.com>
2024-04-03 10:20:51 +0200

    feat: Update module input variables and taskfiles
    - Update module input variable descriptions for is_enabled and tags in the default module.
    - Add additional variable examples in the default module.
    - Add taskfiles for Go linting, formatting, testing, and CI.

--- examples/default/basic/variables.tf
+++ examples/default/basic/variables.tf
@@ -1,8 +1,31 @@
+###################################
+# Input Variables 🛠️
+# ----------------------------------------------------
+#
+# These variables allow users to customize the module according to their needs.
+# Each variable is documented with its description, type, and default value if applicable.
+#
+###################################
+
 variable "is_enabled" {
-  description = "Enable or disable the module"
   type        = bool
+  description = <<-DESC
+  Whether this module will be created or not. Useful for stack-composite
+  modules that conditionally include resources provided by this module.
+  DESC
+  default     = true
 }
 
-###################################
-# Specific for this module
-###################################
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to add to all resources."
+  default     = {}
+}
+
+# Add your additional variables here with a brief description, type, and default value if applicable.
+# Example:
+# variable "queue_name" {
+#   type        = string
+#   description = "The name of the SQS queue to be created."
+#   default     = null
+# }
