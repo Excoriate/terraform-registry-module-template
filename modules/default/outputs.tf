c7d41ab4677c99df07659c2375e75aeda1353e02
Alex Torres R <atorres.ruiz@hotmail.com>
2024-04-03 10:21:42 +0200

    feat: Update module input variables and taskfiles (#8)
    - Update module input variable descriptions for is_enabled and tags in the default module.
    - Add additional variable examples in the default module.
    - Add taskfiles for Go linting, formatting, testing, and CI.

--- modules/default/outputs.tf
+++ modules/default/outputs.tf
@@ -8,9 +8,24 @@ output "tags_set" {
   description = "The tags set for the module."
 }
 
-/*
--------------------------------------
-Custom outputs
--------------------------------------
-*/
-// FIXME: Remove, refactor or change. (Template)
+###################################
+# Module-Specific Outputs 🚀
+# ----------------------------------------------------
+#
+# These outputs are specific to the functionality provided by this module.
+# They offer insights and access points into the resources created or managed by this module.
+#
+# (Add your module-specific outputs here, with a brief description for each)
+#
+###################################
+
+# Example of a module-specific output:
+# output "queue_url" {
+#   value       = aws_sqs_queue.main.url
+#   description = "The URL of the created SQS queue."
+# }
+
+# output "dlq_url" {
+#   value       = aws_sqs_queue.dlq.url
+#   description = "The URL of the created Dead Letter Queue (DLQ)."
+# }
--- modules/default/variables.tf
+++ modules/default/variables.tf
@@ -1,9 +1,19 @@
+###################################
+# Input Variables 🛠️
+# ----------------------------------------------------
+#
+# These variables allow users to customize the module according to their needs.
+# Each variable is documented with its description, type, and default value if applicable.
+#
+###################################
+
 variable "is_enabled" {
   type        = bool
-  description = <<EOF
-  Whether this module will be created or not. It is useful, for stack-composite
-modules that conditionally includes resources provided by this module..
-EOF
+  description = <<-DESC
+  Whether this module will be created or not. Useful for stack-composite
+  modules that conditionally include resources provided by this module.
+  DESC
+  default     = true
 }
 
 variable "tags" {
@@ -12,7 +22,10 @@ variable "tags" {
   default     = {}
 }
 
-###################################
-# Specific for this module
-###################################
-// FIXME: Remove, refactor or change. (Template)
+# Add your additional variables here with a brief description, type, and default value if applicable.
+# Example:
+# variable "queue_name" {
+#   type        = string
+#   description = "The name of the SQS queue to be created."
+#   default     = null
+# }
